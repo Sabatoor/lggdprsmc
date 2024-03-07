83d9044e7b597deda37b57d5667546284255e9a8
Neil Mastroianni <nf_mastroianni@hotmail.com>
2024-03-07 13:55:41 -0500

    removed centered hero content

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -17,59 +17,61 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>
  */
 const Hero = ({ slice, index }: HeroProps): JSX.Element => {
   return (
-    <Section
+    <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className={cn(
-        'relative mx-auto flex items-center justify-center overflow-hidden bg-skin-neutral py-24 md:py-44 lg:h-[calc(100vh-150px)]',
+        'relative mx-auto flex flex-col overflow-hidden px-0 py-0 md:px-0 md:py-0 lg:py-0',
         { 'theme-alternate': slice.primary.theme === 'Alternate' },
       )}
     >
-      <PrismicNextImage
-        field={slice.primary.background_image}
-        className="absolute inset-0 h-full w-full object-cover opacity-[.10]"
-        fill
-        sizes="100vw"
-        priority={index === 0}
-      />
-      {/* <div className="absolute inset-0 h-full w-full bg-gradient-to-br from-skin-hue-primary via-skin-hue-primary to-transparent opacity-90" /> */}
-      <div className="relative mx-auto max-w-screen-xl text-skin-base">
-        <PrismicRichText
-          field={slice.primary.heading}
-          components={{
-            heading1: ({ children }) => (
-              <Heading
-                as="h1"
-                size="7xl"
-                className="my-6 text-skin-base lg:my-10"
-              >
-                {children}
-              </Heading>
-            ),
-          }}
+      <div className="aspect-h-9 aspect-w-16 relative">
+        <PrismicNextImage
+          field={slice.primary.background_image}
+          className="w-full"
+          priority={index === 0}
         />
-        {isFilled.richText(slice.primary.description) && (
-          <span className="relative mb-6 flex justify-center text-skin-muted lg:mb-8">
-            <PrismicRichText field={slice.primary.description} />
-          </span>
-        )}
-        {slice.items.length > 0 && (
-          <div className="flex flex-col justify-center gap-8 lg:flex-row">
-            {isFilled.link(slice.items[0].button_link) &&
-              slice.items.map((item, index) => (
-                <ButtonLink
-                  field={item.button_link}
-                  color={index === 0 ? item.button_color : 'Ghost'}
-                  key={item.button_label}
-                  className="border border-skin-base"
-                >
-                  {item.button_label}
-                </ButtonLink>
-              ))}
-          </div>
-        )}
       </div>
-    </Section>
+      {/* <div className="absolute inset-0 h-full w-full bg-gradient-to-br from-skin-hue-primary via-skin-hue-primary to-transparent opacity-90" /> */}
+      <div className="-mt-12 flex items-center justify-center rounded-t-lg bg-skin-neutral bg-opacity-45 py-4 text-skin-base backdrop-blur-md lg:-mt-24 lg:min-h-[250px] lg:py-8">
+        <div>
+          <PrismicRichText
+            field={slice.primary.heading}
+            components={{
+              heading1: ({ children }) => (
+                <Heading
+                  as="h1"
+                  size="7xl"
+                  className="text-skin-base lg:text-center"
+                >
+                  {children}
+                </Heading>
+              ),
+            }}
+          />
+          {isFilled.richText(slice.primary.description) && (
+            <span className="relative my-6 flex justify-center text-skin-muted lg:mb-8">
+              <PrismicRichText field={slice.primary.description} />
+            </span>
+          )}
+          {slice.items.length > 0 && (
+            <div className="mt-6 flex flex-col justify-center gap-8 lg:mt-8 lg:flex-row">
+              {isFilled.link(slice.items[0].button_link) &&
+                slice.items.map((item, index) => (
+                  <ButtonLink
+                    field={item.button_link}
+                    color={index === 0 ? item.button_color : 'Ghost'}
+                    key={item.button_label}
+                    className="border border-skin-base"
+                  >
+                    {item.button_label}
+                  </ButtonLink>
+                ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
   )
 }
 
