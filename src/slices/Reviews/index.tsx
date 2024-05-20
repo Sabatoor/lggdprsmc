42c6841fa87219ec66cab82afaa5193018a4c9fa
Neil Mastroianni <nf_mastroianni@hotmail.com>
2024-05-20 16:29:29 -0400

    removed console log

--- src/slices/Reviews/index.tsx
+++ src/slices/Reviews/index.tsx
@@ -25,24 +25,22 @@ const Reviews = async ({ slice }: ReviewsProps): Promise<JSX.Element> => {
       },
     },
   ).then(res => res.json())
-  console.log('DATA', data)
   // const data = await fetch(process.env.URL + '/api/reviews', {
   //   method: 'GET',
   //   next: {
   //     revalidate: 60 * 60 * 24 * 7,
   //   },
   // }).then(res => res.json())
+
   return (
     <Section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className="bg-secondary"
     >
-      {!data.error.code && (
-        <Suspense fallback={<div>Loading reviews...</div>}>
-          <Slider reviews={data.reviews} />
-        </Suspense>
-      )}
+      <Suspense fallback={<div>Loading reviews...</div>}>
+        <Slider reviews={data.reviews} />
+      </Suspense>
     </Section>
   )
 }
